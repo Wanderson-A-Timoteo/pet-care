94ffd11176737e4b60de152c8a216edd8135ac94
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2025-11-22 17:39:49 -0400

    refactor: Adiciona PetProvider ao App.js para acesso global ao contexto

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Rotas from './src/navegacao/Rotas';
+import { PetProvider } from './src/contextos/PetContext';
 
 export default function App() {
   return (
-    <>
+    <PetProvider> 
       <StatusBar style="auto" />
       <Rotas />
-    </>
+    </PetProvider>
   );
 }
