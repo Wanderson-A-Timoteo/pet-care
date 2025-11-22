8cb79bdc97fe470dc8e6c16af1059da7d146b89b
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2025-11-22 17:19:57 -0400

    feat: Adiciona tela PerfilUsuario

---
+++ src/telas/PerfilUsuario.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function PerfilUsuario() {
+  return (
+    <SafeAreaView style={estilos.container}>
+      <Text>Tela: Perfil do Usuário</Text>
+    </SafeAreaView>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
